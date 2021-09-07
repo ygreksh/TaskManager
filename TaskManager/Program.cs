65c3faab204f27b613f7302670e9228024924c7a
ygrek <ygreksh@gmail.com>
2021-09-07 16:02:27 +0300

    1 task array

--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace TaskManager
 {
@@ -6,7 +7,19 @@ namespace TaskManager
     {
         static void Main(string[] args)
         {
-            
+            Task[] tasks1 = new Task[3]
+            {
+                new Task(() => Console.WriteLine("First Task")),
+                new Task(() => Console.WriteLine("Second Task")),
+                new Task(() => Console.WriteLine("Third Task"))
+            };
+            foreach (var t in tasks1)
+                t.Start();
+            Task.WaitAll(tasks1); // ожидаем завершения задач 
+     
+            Console.WriteLine("Завершение метода Main");
+ 
+            Console.ReadLine();
         }
     }
 }
