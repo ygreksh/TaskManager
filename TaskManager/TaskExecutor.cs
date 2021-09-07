dddcb0a85aa85994648afaa11e30029f2f314e35
ygrek <ygreksh@gmail.com>
2021-09-07 15:55:51 +0300

    0 empty

---
+++ TaskManager/TaskExecutor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TaskManager
+{
+    public class TaskExecutor : ITaskExecutor
+    {
+        public int Amount { get; }
+        public void Start(int maxConcurrent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Action action)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
